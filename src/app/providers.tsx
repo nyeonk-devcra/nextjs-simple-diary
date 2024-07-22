b914a697290b0a95710eee7a2824c4e412626f5c
nyeonk-devcra <nyeonk@devcra.com>
2024-07-22 14:49:05 +0900

    [ feat ] useReducer()와 useContext()를 사용하여 데이터 상태 관리

---
+++ src/app/providers.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useReducer, ReactNode, useRef, createContext } from "react";
+import { reducer } from "./reducer";
+import { mockData } from "../type/mock";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+interface DiaryState {
+  id: number;
+  date: number;
+  content: string;
+}
+
+interface DiaryDispatch {
+  onCreate: (date: number, content: string) => void;
+  onUpdate: (id: number, date: number, content: string) => void;
+  onDelete: (id: number) => void;
+}
+
+export const DiaryStateContext = createContext<DiaryState[]>([]);
+export const DiaryDispatchContext = createContext<DiaryDispatch>({
+  onCreate: () => {},
+  onUpdate: () => {},
+  onDelete: () => {},
+});
+
+export function Providers({ children }: ProvidersProps) {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(4);
+
+  const onCreate = (date: number, content: string) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        date,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id: number, date: number, content: string) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        date,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id: number) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider
+        value={{
+          onCreate,
+          onUpdate,
+          onDelete,
+        }}
+      >
+        {children}
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
+}
---
+++ src/app/reducer.ts
@@ -0,0 +1,25 @@
+"use client";
+
+import { MockData } from "../type/mock";
+
+function reducer(
+  state: MockData[],
+  action: { type: string; data?: MockData; id?: number }
+) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item: any) =>
+        String(item.id) === String(action.data?.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item: MockData) => String(item.id) !== String(action.id)
+      );
+    default:
+      return state;
+  }
+}
+
+export { reducer };
